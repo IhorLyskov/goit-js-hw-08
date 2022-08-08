21f6ef07e5b4fa18b32c9b44b65922a45bee75d7
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2022-08-08 20:47:10 +0300

    task2 partially

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Vimeo from '@vimeo/player';
 
+const vimeoPlayer = new Vimeo('vimeo-player');
+
+let vimeoPlayerTime = { 'ideoplayer-current-time': 0 };
+
+vimeoPlayer
+  .setCurrentTime(vimeoPlayerTime['ideoplayer-current-time'])
+  .then(function (seconds) {
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'The time was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        console.log('Some other error occurred');
+    }
+  });
+
+vimeoPlayer.on('timeupdate', function (data) {
+  vimeoPlayerTime['ideoplayer-current-time'] = data.seconds;
+  console.log(`on - ${vimeoPlayerTime['ideoplayer-current-time']}`);
+});
+
+vimeoPlayer.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+  console.log(`Start time: ${vimeoPlayerTime['ideoplayer-current-time']}`);
+});
